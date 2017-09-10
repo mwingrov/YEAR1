86f95f5b61735010738bce90eaeb9a35d87a9efb
Mark WINGROVE <mwingrov@e4r19p4.wtc.co.za>
2017-09-10 03:55:13 +0200

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwingrov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/27 16:06:11 by mwingrov          #+#    #+#             */
+/*   Updated: 2017/08/17 16:25:43 by mwingrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+# include "libft.h"
+
+int        get_next_line(const int fd, char **line);
+
+#endif
